76fb1719e7ea31e8616a9d60d9a3bea63efb4777
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2024-05-24 17:52:17 +0100

    Add initial sketch

---
+++ push_swap.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjorge-l <tjorge-l@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 17:30:32 by tjorge-l          #+#    #+#             */
+/*   Updated: 2024/05/24 17:38:10 by tjorge-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		return (1);
+	else if (argc == 2)
+	{
+		// Initialize stack with result from split.
+	}
+	else
+	{
+		// Either several numeric args or
+		// combination of numeric args and string
+		// from which stack will have to be initialized
+		// or process interrupted.
+	}
+	
+	return (0);
+}
