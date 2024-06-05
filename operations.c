b5cf24f48959000ba585173de93a20891496f821
tjorge-l <tjorge-l@student.42lisboa.com>
2024-06-05 16:59:09 +0100

    Sketch sa operation

--- operations.c
+++ operations.c
@@ -6,7 +6,7 @@
 /*   By: tjorge-l <tjorge-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 16:27:23 by tjorge-l          #+#    #+#             */
-/*   Updated: 2024/06/05 16:30:43 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2024/06/05 16:58:50 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 void	sa(t_stack **a)
 {
+	t_stack		*first;
+	t_stack		*second;
+	t_stack		*temp;
+
+	if (stack_size(a) <= 1)
+		return ;
+	first = *a;
+	second = first->next;
+	temp = first;
 	
+	first->next = second->next;
+	first->nbr = second->nbr;
+	first->idx = second->idx;
+	
+	second->next = first;
+	second->nbr = temp->nbr;
+	second->idx = temp->idx;
+
+	*a = second;
 }
