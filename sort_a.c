5a05686a9872fcf7c10a1049e9fb1651b3b478fb
tjorge-l <tjorge-l@student.42lisboa.com>
2024-06-07 11:38:43 +0100

    Enable stack sorting with 3 int vales

--- sort_a.c
+++ sort_a.c
@@ -6,7 +6,7 @@
 /*   By: tjorge-l <tjorge-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 16:54:42 by tjorge-l          #+#    #+#             */
-/*   Updated: 2024/06/07 10:46:24 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2024/06/07 11:28:10 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,36 @@ int	sorted_q(t_stack **s)
 	return (1);
 }
 
+void	sort_three(t_stack	**a)
+{
+	if ((*a)->nbr == get_min(a))
+	{
+		apply_s(a, 'a');
+		apply_r(a, 'a');
+	}
+	else if ((*a)->nbr == get_max(a))
+	{
+		apply_r(a, 'a');
+		if (!sorted_q(a))
+			apply_s(a, 'a');
+	}
+	else
+	{
+		if (get_node_idx(a, get_min(a) == 1))
+			apply_s(a, 'a');
+		else
+			apply_rr(a, 'a');
+	}
+}
+
 void	sort(t_stack **a, t_stack **b)
 {
-	if (!a || !*a || !b )
+	if (!a || !*a || !b)
 		return ;
 	if (stack_size(a) == 2)
 		apply_s(a, 'a');
+	else
+	{
+		sort_three(a);
+	}
 }
