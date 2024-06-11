06b0c302144ce1a6d67790b17a77eabd87e2a490
tjorge-l <tjorge-l@student.42lisboa.com>
2024-06-11 12:09:28 +0100

    Sketch sorting of stack with 5 or more int values

--- sort_a.c
+++ sort_a.c
@@ -6,7 +6,7 @@
 /*   By: tjorge-l <tjorge-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 16:54:42 by tjorge-l          #+#    #+#             */
-/*   Updated: 2024/06/07 13:21:54 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2024/06/11 11:57:24 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,63 @@ void	sort_three(t_stack	**a)
 	}
 }
 
+void	sort_b(t_stack **a, t_stack **b)
+{
+		if (stack_size(a) > 3)
+			apply_pb(a, b);
+		if (stack_size(a) > 3 && !sorted_q(a))
+			apply_pb(a, b);
+		if (!sorted_q(a))
+			sort_three(a);
+
+		// if ((*b)->nbr > get_max(a))
+		// {
+		// 	apply_pa(a, b);
+		// 	apply_r(a, 'a');
+		// }
+		// else if ((*b)->nbr < get_min(a))
+		// 	apply_pa(a, b);
+		// else if ((*b)->nbr < (*a)->next->nbr)
+		// {
+		// 	apply_pa(a, b);
+		// 	apply_s(a, 'a');
+		// }
+		// else
+		// {
+		// 	apply_pa(a, b);
+		// 	apply_rr(a, 'a');
+		// 	apply_s(a, 'a');
+		// 	apply_r(a, 'a');
+		// 	apply_r(a, 'a');
+		// }
+}
+
+void	sort_a(t_stack **a, t_stack **b)
+{
+	int		i;
+	t_stack	*temp;
+
+	while (*b)
+	{
+		temp = *b;
+
+		i = best_nbr_moves_ba(a, b);
+		while (i >= 0)
+		{
+			// if (i == case_rarb_a(a, b, temp->nbr))
+			// 	i = apply_rarb(a, b, temp->nbr, 'b');
+			// else if (i == case_rarrb_a(a, b, temp->nbr))
+			// 	i = apply_rarrb(a, b, temp->nbr, 'b');
+			// else if (i == case_rrarrb_a(a, b, temp->nbr))
+			// 	i = apply_rrarrb(a, b, temp->nbr, 'b');
+			// else if (i == case_rrarb_a(a, b, temp->nbr))
+			// 	i = apply_rrarb(a, b, temp->nbr, 'b');
+			// else
+			// 	temp = temp->next;
+		}
+	}
+}
+
 void	sort(t_stack **a, t_stack **b)
 {
 	if (!a || !*a || !b)
@@ -87,5 +144,10 @@ void	sort(t_stack **a, t_stack **b)
 			apply_r(a, 'a');
 			apply_r(a, 'a');
 		}
+	}
+	else if (stack_size(a) == 5)
+	{
+		sort_b(a, b);
+		sort_a(a, b);
 	}
 }
