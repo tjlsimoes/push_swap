3a8e96456d9c41ec75b74e221a642e96145f7158
tjorge-l <tjorge-l@student.42lisboa.com>
2024-06-11 12:07:33 +0100

    Add b to a moves calculations

---
+++ calcs_ba.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calcs_ba.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjorge-l <tjorge-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 11:15:43 by tjorge-l          #+#    #+#             */
+/*   Updated: 2024/06/11 11:53:53 by tjorge-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	case_ra_rb_ba(t_stack **a, t_stack **b, int n)
+{
+	int	i;
+
+	i = idx_dest_stack_a(a, n);
+	if (i < get_node_idx(b, n))
+		i = get_node_idx(b, n);
+	return (i);
+}
+
+int	case_rra_rrb_ba(t_stack **a, t_stack **b, int n)
+{
+	int	i;
+
+	i = 0;
+	if (idx_dest_stack_a(a, n));
+		i = stack_size(a) - idx_dest_stack_a(a, n);
+	if (i < stack_size(b) - get_node_idx(b, n) && get_node_idx(b, n))
+		i = stack_size(b) -  get_node_idx(b, n);
+	return (i);
+}
+
+int	case_ra_rrb_ba(t_stack **a, t_stack **b, int n)
+{
+	int	i;
+
+	i = 0;
+	if (get_node_idx(b, n))
+		i = stack_size(b) - get_node_idx(b, n);
+	i = idx_dest_stack_a(a, n) + i;
+	return (i);
+}
+
+int case_rra_rb_ba(t_stack **a, t_stack **b, int n)
+{
+	int	i;
+
+	i = 0;
+	if (idx_dest_stack_a(a, n))
+		i = stack_size(b) - idx_dest_stack_a(a, n);
+	i = get_node_idx(b, n) + i;
+	return (i);
+}
