e2b239c64c559b98a588a30db40b6a51ab872dff
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2024-06-12 14:55:13 +0100

    Remove unnecessary comments

--- operations_d.c
+++ operations_d.c
@@ -6,7 +6,7 @@
 /*   By: tjorge-l <tjorge-l@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 13:59:58 by tjorge-l          #+#    #+#             */
-/*   Updated: 2024/06/12 13:45:39 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2024/06/12 13:55:49 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int	apply_ra_rb(t_stack **a, t_stack **b, int n, char c)
 	{
 		while ((*a)->nbr != n && idx_dest_stack_b(b, n) > 0)
 			apply_rr_ab(a, b);
-		// Why idx_dest_stack_a(a, n) ?? Ensure min is on top?
 		while ((*a)->nbr != n)
 			apply_r(a, 'a');
 		while (idx_dest_stack_b(b, n) > 0)
@@ -29,7 +28,6 @@ int	apply_ra_rb(t_stack **a, t_stack **b, int n, char c)
 	{
 		while ((*b)->nbr != n && idx_dest_stack_a(a, n) > 0)
 			apply_rr_ab(a, b);
-		// Why idx_dest_stack_a(a, n) ?? Ensure min is on top?
 		while ((*b)->nbr != n)
 			apply_r(b, 'b');
 		while (idx_dest_stack_a(a, n) > 0)
@@ -39,13 +37,16 @@ int	apply_ra_rb(t_stack **a, t_stack **b, int n, char c)
 	return (-1);
 }
 
+// idx_dest_stack_a(a, n) > 0 ensures that nbr to be pushed
+// will be placed in its correct position when pushed to the
+// top of the respective stack.
+
 int	apply_rra_rrb(t_stack **a, t_stack **b, int n, char c)
 {
 	if (c == 'a')
 	{
 		while ((*a)->nbr != n && idx_dest_stack_b(b, n) > 0)
 			apply_rrr(a, b);
-		// Why idx_dest_stack_a(a, n) ?? Ensure min is on top?
 		while ((*a)->nbr != n)
 			apply_rr(a, 'a');
 		while (idx_dest_stack_b(b, n) > 0)
@@ -56,7 +57,6 @@ int	apply_rra_rrb(t_stack **a, t_stack **b, int n, char c)
 	{
 		while ((*b)->nbr != n && idx_dest_stack_a(a, n) > 0)
 			apply_rrr(a, b);
-		// Why idx_dest_stack_a(a, n) ?? Ensure min is on top?
 		while ((*b)->nbr != n)
 			apply_rr(b, 'b');
 		while (idx_dest_stack_a(a, n) > 0)
