72fd6a191bfb8153cbd335e04a1b720dba4039fb
tjorge-l <tjorge-l@student.42lisboa.com>
2024-06-14 13:32:06 +0100

    Modularize rrr, rr_ab and ss operations

--- operations_c.c
+++ operations_c.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   operations_c.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjorge-l <tjorge-l@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: tjorge-l <tjorge-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 12:24:07 by tjorge-l          #+#    #+#             */
-/*   Updated: 2024/06/12 13:46:09 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2024/06/14 12:21:07 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	rrr(t_stack **a, t_stack **b)
+{
+	rr(a);
+	rr(b);
+}
 
 void	apply_rrr(t_stack **a, t_stack **b)
 {
@@ -19,11 +25,23 @@ void	apply_rrr(t_stack **a, t_stack **b)
 	ft_printf("rrr\n");
 }
 
+void	rr_ab(t_stack **a, t_stack **b)
+{
+	r(a);
+	r(b);
+}
+
 void	apply_rr_ab(t_stack **a, t_stack **b)
 {
 	r(a);
 	r(b);
 	ft_printf("rr\n");
+}
+
+void	ss(t_stack **a, t_stack **b)
+{
+	s(a);
+	s(b);
 }
 
 void	apply_ss(t_stack **a, t_stack **b)
