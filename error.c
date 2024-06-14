d7dd119e306c8d5bbba18b1f6fe28cd8e8a422d1
tjorge-l <tjorge-l@student.42lisboa.com>
2024-06-14 13:31:22 +0100

    Enable free and exit for moves' parsing

--- error.c
+++ error.c
@@ -1,16 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_error.c                                         :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjorge-l <tjorge-l@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: tjorge-l <tjorge-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 15:22:01 by tjorge-l          #+#    #+#             */
-/*   Updated: 2024/06/04 16:12:04 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2024/06/14 12:34:10 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	free_lab_error(t_list **moves, t_stack **a, t_stack **b)
+{
+	alt_lstclear(moves);
+	free_stack(a);
+	free_stack(b);
+	ft_error();
+}
 
 void	free_and_error(t_stack **a)
 {
